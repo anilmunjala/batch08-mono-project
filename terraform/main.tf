342b7f77f7011df9e18f4d5345426787286c0056
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-14 10:22:08 +0530

    update pem

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "key" {
 ami = "ami-005f9685cb30f234b"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
-key_name = "my-project-kp"
+key_name = "mypem"
 tags = {
 Name = "project-instance"
 Environment = "Prod"
