c1b058ca5af6b5fae05a842f3cc179a4c1c5ad81
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-14 10:27:42 +0530

    update credits

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIARSPNELGYF6H52P3Z"
-secret_key = "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"
+access_key = "AKIARSPNELGYNNWRGVVL"
+secret_key = "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"
 }
